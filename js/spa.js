ad04a16057a34afb9237c624e6e8f2b866df456a
tomowill <tomo144@gmail.com>
2016-10-11 01:34:10 +0900

    advanced to 5-1

---
+++ js/spa.js
@@ -0,0 +1,15 @@
+/*jslint       browser : true, continue : true,
+ devel  : true, indent : 2,    maxerr : 50,
+ newcap : true, nomen  : true, plusplus:true,
+ regexp : true, sloppy : true, vars : false,
+ white : true
+*/
+/*global $, spa:true */
+
+var spa = (function () {
+  var initModule = function ( $container ){
+    spa.shell.initModule( $container );
+  };
+
+  return { initModule: initModule };
+}());
